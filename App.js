a007e9344c9ef444cc496247a6d0de1700294624
Jesudoss <jesudoss2107@gmail.com>
2024-03-13 12:45:52 +0530

    jsx learn

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const heading=React.createElement("h1",{className:"headermae"},"heading 1");
+// const heading=React.createElement("h1",{className:"headermae"},"heading 1");
+const Heading=()=>{
+    return (
+    <h1 id="h1" key="h1">hello jesu</h1>
+    );
+};
+
+const Heading2=()=>{
+    return (
+    <>
+    {/* <Heading/> one way */}
+    {/* {Heading()} second way because internally it is a js function */}
+    <h1 id="h1" key="h1">hellojs jesu</h1>
+    </> 
+    );
+};
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<Heading2/>);
+
 
 
 
